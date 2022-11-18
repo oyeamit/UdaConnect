40b18073c1586c8e67b10ff820cddd57a4d386ad
Amit Manbansh <amanbansch@altagram.com>
2022-11-19 00:12:07 +0100

    refactor Persons API

---
+++ modules/persons_api/app/udaconnect/models.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from app import db  # noqa
+from sqlalchemy import BigInteger, Column, Date, DateTime, ForeignKey, Integer, String
+from sqlalchemy.dialects.postgresql import JSONB, UUID
+from sqlalchemy.ext.hybrid import hybrid_property
+
+
+class Person(db.Model):
+    __tablename__ = "person"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    company_name = Column(String, nullable=False)
+
