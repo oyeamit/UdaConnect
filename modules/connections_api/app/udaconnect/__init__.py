a83af99dd6d3acb078b97f35db9fa9f7291daa24
Amit Manbansh <amanbansch@altagram.com>
2022-11-19 13:05:58 +0100

    corrected api names in __init__.py

--- modules/connections_api/app/udaconnect/__init__.py
+++ modules/connections_api/app/udaconnect/__init__.py
@@ -1,8 +1,8 @@
-from app.udaconnect.models import Connection, Location, Person  # noqa
-from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema  # noqa
+from app.udaconnect.models import Connection  # noqa
+from app.udaconnect.schemas import ConnectionSchema  # noqa
 
 
 def register_routes(api, app, root="api"):
-    from app.udaconnect.controllers import api as udaconnect_api
+    from app.udaconnect.controllers import api as connections_api
 
-    api.add_namespace(udaconnect_api, path=f"/{root}")
+    api.add_namespace(connections_api, path=f"/{root}")
